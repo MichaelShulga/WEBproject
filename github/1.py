6e06ad1d938c4594cac13c77e71c15c1ad717e1e
MichaelShulga <anatemsf@yandex.ru>
2021-04-24 13:22:28 +0300

    msg

---
+++ github/1.py
@@ -0,0 +1,36 @@
+from time import *
+from os import path
+from git import Repo
+import os
+
+curr_dir = os.path.dirname(os.path.realpath(__file__)).replace('github', '')
+print(curr_dir)
+repo = Repo(curr_dir)
+
+
+def commit_files():
+    if repo != None:
+        new_branch = 'your_new_branch'
+        current = repo.create_head(new_branch)
+        current.checkout()
+        master = repo.heads.master
+        repo.git.pull('origin', master)
+        # creating file
+        dtime = strftime('%d-%m-%Y %H:%M:%S', localtime())
+        with open(curr_dir + path.sep + 'lastCommit' + '.txt', 'w') as f:
+            f.write(str(dtime))
+        if not path.exists(curr_dir):
+            os.makedirs(curr_dir)
+        print('file created---------------------')
+
+        if repo.index.diff(None) or repo.untracked_files:
+
+            repo.git.add(A=True)
+            repo.git.commit(m='msg')
+            repo.git.push('--set-upstream', 'origin', current)
+            print('git push')
+        else:
+            print('no changes')
+
+
+commit_files()
---
+++ test.py
@@ -0,0 +1,2 @@
+a = 'sfdf'
+print(a.replace('s', 'S'))
